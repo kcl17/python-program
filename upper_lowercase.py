337c834d909535773a44909539519505adb432d2
khushi798 <120015301+khushi798@users.noreply.github.com>
2022-12-10 10:45:05 +0530

    Add files via upload

---
+++ upper_lowercase.py
@@ -0,0 +1,7 @@
+text = str(input("Enter your character: "))
+
+if(text.isupper()):
+    print("The given character", text, "is upper case")
+
+elif(text.islower()):
+    print("The given character", text, "is lower case")
