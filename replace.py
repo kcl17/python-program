90c6bc99ac50e1afd416808382d7d62f1dcfd3ae
khushi798 <120015301+khushi798@users.noreply.github.com>
2022-12-10 11:25:53 +0530

    Update replace.py

--- replace.py
+++ replace.py
@@ -1,6 +1,6 @@
 string = str(input("Enter a string: "))
  
-new_string = string.replace(input("Enter the character to replace: "), input("Enter the character to replace with: ") )
+new_string = string.replace(input("Enter the word to replace: "), input("Enter the word to replace with: ") )
  
 print(string)
 print(new_string)
