5510f3d4e029d06ccc4a802a516c6b819380dfaa
Khushi chhillar <120015301+khushi798@users.noreply.github.com>
2023-01-28 21:05:03 +0530

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,22 +1,16 @@
-def eqn(a,b,c):
-    global det
-    det = b**2 - 4*a*c
-    if det == 0 or det > 0:
-        x1 = float((-b + (det)**0.5)/2*a)
-        x2 = float((-b - (det)**0.5)/2*a)
-        return x1, x2
-        
-    elif det < 0:
-        return "No real solutions"
-l = True
-while l == True:
-    a = int(input("Enter the co-efficient of x\N{SUPERSCRIPT TWO}: "))
-    b = int(input("Enter the co-efficient of x: "))
-    c = int(input("Enter the constant term: "))
-    print(eqn(a,b,c))
-    l = input("Do you want to run the program again? (y/n): ")
-    if l == "y":
-        l = bool(1)
+def find_roots(a, b, c):
+    discriminant = b**2 - 4*a*c
+
+    if discriminant >= 0:
+
+        sqrt_discriminant = discriminant**0.5
+        root1 = (-b + sqrt_discriminant) / (2*a)
+        root2 = (-b - sqrt_discriminant) / (2*a)
+        return (root1, root2)
     else:
-        l = bool(0)
-    
+
+        real_part = -b / (2*a)
+        imag_part = discriminant**0.5 / (2*a)
+        return (real_part + imag_part*1j, real_part - imag_part*1j)
+
+print(find_roots(1, -3, 2))  
